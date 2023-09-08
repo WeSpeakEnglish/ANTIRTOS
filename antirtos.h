7db904bebccae2378cafb248dae6a4b83d63ebb0
Aleksei Tertychnyi <cqdx@list.ru>
2023-09-08 12:55:22 +0200

    MKR WAN EXAMPLES added

--- antirtos.h
+++ antirtos.h
@@ -1,16 +1,22 @@
+// here are ANTIRTOS two classes 
+// -first fQ for functions without parameters, the second with parameter 
+// you could easily initialize your queue like fQ F1(10); // 10 - here is your desired lenght of task queue, F1 - you may replace with your name - name of queue 
+// in case you plan to pass parameters to functions too: fQp<yourClass> F2(10); // yourClass -could be your defined class or standard class (ex int, uint16_t... etc ), 10 - here is lenght of task queue, F2 - you may replace with your name - name of queue  
+// push method is pushing your function (or function and parameter ), example: F1.push(yourFunctionName); 
+// in case we have functions with parameters: F2.push(yourFunctionName, parameter);
+// pull function pulling and execute the task from queue, example: F1.pull(); F2.pull;
+// under MIT license, Aleksei Tertychnyi
+
 #ifndef antirtos_h
 #define antirtos_h
 
-typedef void(*fP)(void);
-
-
-/// @brief class for finctional pointers queue with supporting of transfer of parameters
-
+/// @brief class for finctional pointers queue without parameters
 class fQ {
 private:
     volatile int first;
     volatile int last;
     int length;
+	typedef void(*fP)(void);
     fP * fQueue;
 public:
     fQ(int sizeQ);
@@ -32,7 +38,7 @@ fQ::~fQ(){ // initialization of Queue
 
 int fQ::push(fP pointerF){ // push element from the queue
   if ((last+1)%length == first){
-            return 1;
+    return 1;
   }
   fQueue[last++] = pointerF;
   last = last%length;
@@ -50,15 +56,15 @@ int fQ::pull(void){ // pull element from the queue
   }
 };
 
-
+/// @brief class for finctional pointers queue with supporting of transfer of parameters
 template <typename T>
 class fQP {
 private:
     volatile int first;
     volatile int last;
     int length;
-    typedef void (*FunctionPointer)(T);
-    FunctionPointer* FP_Queue;
+    typedef void (*fP)(T);
+    fP* FP_Queue;
     T* PARAMS_array;
 
 public:
@@ -70,7 +76,7 @@ public:
 
 template <typename T>
 fQP<T>::fQP(int sizeQ) {
-    FP_Queue = new FunctionPointer[sizeQ];
+    FP_Queue = new fP[sizeQ];
     PARAMS_array = new T[sizeQ];
     last = 0;
     first = 0;
@@ -94,7 +100,7 @@ int fQP<T>::push(void (*pointerQ)(T), T parameterQ) {
 
 template <typename T>
 int fQP<T>::pull() {
-    FunctionPointer pullVar;
+    fP pullVar;
     if (last != first) {
         T Params = PARAMS_array[first];
         pullVar = FP_Queue[first];
