b97d7221e4a5e3407dc3e2659cfddd60c7204437
Aleksei Tertychnyi <cqdx@list.ru>
2024-07-22 14:16:13 +0200

    fQ returned back

--- antirtos.h
+++ antirtos.h
@@ -10,6 +10,109 @@
 #ifndef antirtos_h
 #define antirtos_h
 
+/// @brief class for finctional pointers queue without parameters
+class fQ {
+private:
+    int first;
+    volatile int last;
+    int length;
+	typedef void(*fP)(void);
+    fP * fQueue;
+public:
+    fQ(int sizeQ);
+    ~fQ();
+    int push(fP);
+    int pull(void);
+};
+
+fQ::fQ(int sizeQ){ // initialization of Queue
+  fQueue = new fP[sizeQ];
+  last = 0;
+  first = 0;
+  length = sizeQ;
+};
+
+fQ::~fQ(){ // initialization of Queue
+  delete [] fQueue;
+};
+
+int fQ::push(fP pointerF){ // push element from the queue
+  if ((last+1)%length == first){
+    return 1;
+  }
+  fQueue[last++] = pointerF;
+  last = last%length;
+  return 0;
+};
+
+int fQ::pull(void){ // pull element from the queue
+  if (last != first){
+  fQueue[first++]();
+  first = first%length;
+  return 0;
+  }
+  else{
+   return 1;
+  }
+};
+
+/// @brief class for functional pointers queue with supporting of transfer of parameters
+template <typename T>
+class fQP {
+private:
+    int first;
+    volatile int last;
+    int length;
+    typedef void (*fP)(T);
+    fP* FP_Queue;
+    T* PARAMS_array;
+
+public:
+    fQP(int sizeQ);
+    ~fQP();
+    int push(void (*pointerQ)(T), T parameterQ);
+    int pull();
+};
+
+template <typename T>
+fQP<T>::fQP(int sizeQ) {
+    FP_Queue = new fP[sizeQ];
+    PARAMS_array = new T[sizeQ];
+    last = 0;
+    first = 0;
+    length = sizeQ;
+}
+
+template <typename T>
+fQP<T>::~fQP() {
+    delete[] FP_Queue;
+    delete[] PARAMS_array;
+}
+
+template <typename T>
+int fQP<T>::push(void (*pointerQ)(T), T parameterQ) {
+    if ((last + 1) % length == first) return 1;
+    FP_Queue[last] = pointerQ;
+    PARAMS_array[last] = parameterQ;
+    last = (last + 1) % length;
+    return 0;
+}
+
+template <typename T>
+int fQP<T>::pull() {
+    fP pullVar;
+    if (last != first) {
+        T Params = PARAMS_array[first];
+        pullVar = FP_Queue[first];
+        first = (first + 1) % length;
+        pullVar(Params);
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
 /// @brief delayed functional pointers queue without parameters, only time delay parameter
 class del_fQ{
 private:
@@ -99,62 +202,4 @@ int del_fQ::pull(void){ // pull element from the queue
    return 1;
   }
 };
-
-/// @brief class for functional pointers queue with supporting of transfer of parameters
-template <typename T>
-class fQP {
-private:
-    int first;
-    volatile int last;
-    int length;
-    typedef void (*fP)(T);
-    fP* FP_Queue;
-    T* PARAMS_array;
-
-public:
-    fQP(int sizeQ);
-    ~fQP();
-    int push(void (*pointerQ)(T), T parameterQ);
-    int pull();
-};
-
-template <typename T>
-fQP<T>::fQP(int sizeQ) {
-    FP_Queue = new fP[sizeQ];
-    PARAMS_array = new T[sizeQ];
-    last = 0;
-    first = 0;
-    length = sizeQ;
-}
-
-template <typename T>
-fQP<T>::~fQP() {
-    delete[] FP_Queue;
-    delete[] PARAMS_array;
-}
-
-template <typename T>
-int fQP<T>::push(void (*pointerQ)(T), T parameterQ) {
-    if ((last + 1) % length == first) return 1;
-    FP_Queue[last] = pointerQ;
-    PARAMS_array[last] = parameterQ;
-    last = (last + 1) % length;
-    return 0;
-}
-
-template <typename T>
-int fQP<T>::pull() {
-    fP pullVar;
-    if (last != first) {
-        T Params = PARAMS_array[first];
-        pullVar = FP_Queue[first];
-        first = (first + 1) % length;
-        pullVar(Params);
-        return 0;
-    }
-    else {
-        return 1;
-    }
-}
-
 #endif
